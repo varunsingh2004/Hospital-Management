123af48caf1a0fed35d5713dc56933aa0a9dd5ed
Varun Singh <singhvarun1904@gmail.com>
2025-04-06 23:40:24 +0530

    new commit 3

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 
 // Configure CORS
 const corsOptions = {
-  origin: process.env.NODE_ENV === 'production' 
+  origin: process.env.NODE_ENV === 'Production' 
     ? ['https://hospital-management-kappa-seven.vercel.app'] // Add your production domain
     : ['http://localhost:3000', 'http://127.0.0.1:3000','http://localhost:5172','http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'http://localhost:5176', 'http://127.0.0.1:5174'], // Development domains
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
